8a2e81c0366ef3e0419262df3803335e6b4000f5
jfmontealegre <127462534+jfmontealegre@users.noreply.github.com>
2025-07-08 15:51:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ if not st.session_state["autenticado"]:
     st.markdown("</div>", unsafe_allow_html=True)
     st.stop()
 
+# BLOQUE CORREGIDO
 st.markdown("""
 <style>
     html, body, [class*="css"] {
@@ -100,7 +101,6 @@ st.markdown("""
     }
 </style>
 """, unsafe_allow_html=True)
-"""
 
 credenciales = {
     "admin": {"password": "1234", "centros": ["52000", "52010", "52012", "51000", "51010"]},
